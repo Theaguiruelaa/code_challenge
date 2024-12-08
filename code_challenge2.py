32e5c96778aeabae4d7f4fdf8280935bf62ee784
Theaguiruelaa <theaguiruela@gmail.com>
2024-12-08 09:00:53 +0800

    Add files via upload

---
+++ code_challenge2.py
@@ -0,0 +1,2 @@
+name = input ("What is your name? ")
+print ("\t\t\t\t\t\t\t\t\t\t\t\t* \n\t\t\t\t\t\t\t\t\t\t\t\b* * * \n\t\t\t\t\t\t\t\t\t\t\t\b\b\b* * * * * \n\t\t\t\t\t\t\t\t\t\t\t\b\b\b\b* Hi," + name + " *")
