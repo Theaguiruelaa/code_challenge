f47bd32d4b7695bbc9a5d46343958e96090c3911
Theaguiruelaa <theaguiruela@gmail.com>
2024-12-14 16:29:47 +0800

    Add files via upload

---
+++ code_challenge5.py
@@ -0,0 +1,5 @@
+print("Fahrenheit to Celsius Converter Program")
+print("\n--------------------------------------")
+
+Fahrenheit = eval(input("\nEnter temparature to fahrenheit: "))
+Celsius = ((Fahrenheit - 32)* 5)
